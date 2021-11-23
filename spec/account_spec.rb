d0f4008e34be3a5b6104f1d4b6a84608e966a539
Tim Turvey <tturvey@hotmail.co.uk>
2021-11-23 12:24:20 +0000

    Withdraw method created and test passed

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -17,4 +17,13 @@ describe Account do
     end
   end
 
+  describe '#withdraw' do
+    it 'minuses a specified amount from the account balance' do
+      account = Account.new
+      account.deposit(100)
+      account.withdraw(25)
+      expect(account.balance).to eq(75)
+    end
+  end
+
 end
